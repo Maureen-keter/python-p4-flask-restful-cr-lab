ebdef596d09e58aff572c339cb61b1733498f6f3
maureen <maureen.chelangat@student.moringaschool.com>
2024-01-13 16:57:50 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -16,7 +16,65 @@ db.init_app(app)
 
 api = Api(app)
 
+@app.route('/plants')
+def get_plants():
+    plants_list=[]
+    for plant in Plant.query.all():
+        plants_dict={
+            "id":plant.id,
+            "name":plant.name,
+            "image":plant.image,
+            "price":plant.price            
+        }
+        plants_list.append(plants_dict)
+        response=make_response(jsonify(plants_list), 200)
+        response.headers['Content-type']='application/json'
+        return response
+@app.route('/plants/<int:id>')
+def get_plants_by_id(id):
+    plant_item=[]
+    plant=Plant.query.filter_by(id=id).first()
+    if plant:
+        plant_dict={
+            "id":plant.id,
+            "name":plant.name,
+            "image":plant.image,
+            "price":plant.price            
+        }
+        plant_item.append(plant_dict)
+        response=make_response(jsonify(plant_dict), 200)
+        response.headers['Content-Type']='application/json'
+        return response
+    else:
+        response_body={'error':'Plant not found'}
+        response=make_response(jsonify(response_body), 404)
+        response.headers['Content-Type']='application.json'
+        return response
+    
+@app.route('/plants', methods=['GET', 'POST'])
+def create_plant():
+    if request.method == 'GET':
+        plants = [plant.to_dict() for plant in Plant.query.all()]
+        response = make_response(jsonify(plants), 200)
+    elif request.method == 'POST':
+            new_plant = Plant(
+                name=request.form.get('name'),
+                image=request.form.get('image'),
+                price=request.form.get('price')
+            )
+            db.session.add(new_plant)
+            db.session.commit()
+            plant_dict = new_plant.to_dict()
+            response = make_response(jsonify(plant_dict), 201) 
+    
+    return response
+ 
+
 class Plants(Resource):
+    def get(self):
+        plants_dict=Plant.query.all()
+        response=make_response(jsonify(plants_dict), 200)
+        return response
     pass
 
 class PlantByID(Resource):
